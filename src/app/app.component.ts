cb170a978667240d2f5a78d93af2dac29a0f6dab
Aaron Roller <aaron.roller@aawhere.com>
2018-12-28 11:41:17 -0800

    content site created ready to share

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'auto-eyes';
-
-  whitePaper() {
-    window.location.href = 'https://docs.google.com/document/d/1lKIsqMYYO7nQ937QXdCg2oaPqeo0iI2x5D2HwVKsVNE/edit?usp=sharing';
-  }
+  title = 'Auto Eyes';
 }
