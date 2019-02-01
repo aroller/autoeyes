284c2b2d1f90a15e15f45925b77dcbda97427295
Aaron Roller <aaron.roller@aawhere.com>
2019-01-31 17:51:47 -0800

    calling api with the bearing.

--- client/compass/compass.js
+++ client/compass/compass.js
@@ -6,6 +6,12 @@ let gamma;
 let bearing;
 let heading;
 
+let lastBearingSent = 0;
+let lastTimestampSent = 0;
+const urlParams = new URLSearchParams(window.location.search);
+let host = urlParams.get('host');
+const apiUrl = `http://${host}:9090/v1.0`
+
 function initialize() {
     const capableElement = document.getElementById('capable');
 
@@ -28,6 +34,12 @@ function initialize() {
             document.getElementById('bearing').innerText = Math.floor(bearing).toString();
             document.getElementById('beta').innerText = Math.floor(beta).toString();
             document.getElementById('gamma').innerText = Math.floor(gamma).toString();
+
+            if(bearing !== lastBearingSent && Date.now() - lastTimestampSent > 500){
+                lastTimestampSent = Date.now();
+                lastBearingSent = bearing;
+                send('p',bearing);
+            }
         }, false);
     } else {
         capableElement.innerHtml = "False";
